af0e54617b7f7c57ae233e675ee2f45265001171
Ayush Choudhary <ayush.choudhary2025@vitstudent.ac.in>
2025-12-07 18:32:53 +0530

    Create sumArray.c
    basic

---
+++ sumArray.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+int a[n],sum = 0;
+scanf("%d", &n);
+for(int i =0; i < n; i++) {
+    scanf("%d", &a[i]);
+    sum = sum+ a[i];
+}
+printf("sum is %d", sum);
+
+}
+
