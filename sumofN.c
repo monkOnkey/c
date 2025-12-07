dc51511f4e03f259cf42f07c151dfa2893543b6b
Ayush Choudhary <ayush.choudhary2025@vitstudent.ac.in>
2025-12-07 14:06:34 +0530

    Create sumofN.c
    basic

---
+++ sumofN.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int sum= 0;
+    printf("enter the value of n");
+    scanf("%d", &n);
+    
+    for ( int i = 0; i <=n ; i++) {
+        sum = sum+i;
+        
+    }
+printf("%d \n", sum);
+
+}
